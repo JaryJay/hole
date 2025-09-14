fcabc4db16c8d9ff5cc7e9567b1bb26ac2e10fbb
Jay <jay.jrjren@gmail.com>
2025-09-14 00:33:51 -0400

    feat: smooth camera controller

--- app/components/CameraController.tsx
+++ app/components/CameraController.tsx
@@ -1,28 +1,46 @@
 "use client";
 
-import { useThree } from "@react-three/fiber";
-import { useEffect } from "react";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useState } from "react";
+import { lerp } from "three/src/math/MathUtils.js";
+
+type Vector3 = [number, number, number];
 
 interface CameraControllerProps {
-	cameraPosition: [number, number, number];
-	groundPosition: [number, number, number];
+  cameraPosition: Vector3;
+  groundPosition: Vector3;
 }
 
 export default function CameraController({
-	cameraPosition,
-	groundPosition,
+  cameraPosition,
+  groundPosition,
 }: CameraControllerProps) {
-	const { camera } = useThree();
+  const { camera } = useThree();
+  const [actualPosition, setActualPosition] = useState<Vector3>([0, 0, 0]);
 
-	useEffect(() => {
-		camera.position.set(
-			cameraPosition[0],
-			cameraPosition[1],
-			cameraPosition[2]
-		);
-		camera.lookAt(groundPosition[0], groundPosition[1], groundPosition[2]);
-		camera.updateProjectionMatrix();
-	}, [camera, cameraPosition, groundPosition]);
+  useFrame(() => {
+    const newActualPosition: Vector3 = [
+      lerp(actualPosition[0], cameraPosition[0], 0.1),
+      lerp(actualPosition[1], cameraPosition[1], 0.1),
+      lerp(actualPosition[2], cameraPosition[2], 0.1),
+    ];
+    // lerp the actual position to the camera position
+    setActualPosition(newActualPosition);
+    camera.position.set(
+      newActualPosition[0],
+      newActualPosition[1],
+      newActualPosition[2]
+    );
 
-	return null;
+    // Place to look at is the ground - target + actual
+    const lookAtPosition: Vector3 = [
+      groundPosition[0] - cameraPosition[0] + newActualPosition[0],
+      groundPosition[1] - cameraPosition[1] + newActualPosition[1],
+      groundPosition[2] - cameraPosition[2] + newActualPosition[2],
+    ];
+    camera.lookAt(lookAtPosition[0], lookAtPosition[1], lookAtPosition[2]);
+    camera.updateProjectionMatrix();
+  });
+
+  return null;
 }
